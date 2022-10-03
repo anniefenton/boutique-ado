99225ec3382af1343f537336a0c4c33f6f123020
anniefenton <anniefentoncontact@gmail.com>
2022-10-03 11:48:10 +0000

    added products views and templates

--- products/admin.py
+++ products/admin.py
@@ -7,7 +7,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
         'name',
-        'caegory',
+        'category',
         'price',
         'rating',
         'image',
